489f0abb31ee90726a7ca8b6a967904414cce40d
Kanbe Hiroyuki <kanbe.hmw@gmail.com>
2020-03-02 01:16:59 +0900

    5_B

---
+++ AOJ/ITP1/5_B.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    while (true) {
+        int H, W;
+        cin >> H >> W;
+        // cout << endl;
+
+        if (H == 0 && W == 0) {
+            break;
+        }
+
+        for (int h = 0; h < H; h++) {
+            for (int w = 0; w < W; w++) {
+                if (h == 0 || h == H - 1 || w == 0 || w == W - 1) {
+                    cout << "#";
+                } else {
+                    cout << ".";
+                }
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
