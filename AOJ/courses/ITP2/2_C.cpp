47159661bcc4acd534e5ca57849c4018c141e4ae
kambehmw <kanbe.hmw@gmail.com>
2020-03-06 20:11:24 +0900

    2_C

---
+++ AOJ/courses/ITP2/2_C.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    priority_queue<int> que[n];
+    for (int i = 0; i < q; i++) {
+        int query;
+        cin >> query;
+        if (query == 0) {
+            int t, x;
+            cin >> t >> x;
+            que[t].push(x);
+        } else if (query == 1) {
+            int t;
+            cin >> t;
+            if (!que[t].empty()) {
+                cout << que[t].top() << endl;
+            }
+        } else if (query == 2) {
+            int t;
+            cin >> t;
+            if (!que[t].empty()) {
+                que[t].pop();
+            }
+        }
+    }
+    return 0;
+}
